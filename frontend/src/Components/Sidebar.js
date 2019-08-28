65f20e211f9ab39f82e7d5910bb48c30286c675b
Stepan <s_mulki@mail.ru>
2019-08-28 14:58:42 +0300

    easyStyles

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -2,6 +2,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter as  NavLink, Link } from "react-router-dom";
+import Wis from '../4.png'
+import Vk from '../5.png'
+import Face from '../6.png'
+import Inst from '../7.png'
 
 export class Sidebar extends Component {
 
@@ -9,10 +13,10 @@ export class Sidebar extends Component {
     return (
       <div className = "sideBar">
         <ul>
-       <li><Link to="/servicePosts" >main</Link></li>
-       <li><Link to="/VK">VK</Link></li>
-       <li><Link to="/facebook">facebook</Link></li>
-       <li><Link to="/instagram">instagram</Link></li>
+       <li><Link to="/servicePosts" ><img src = {Wis} width = "250px"/></Link></li>
+       <li><Link to="/VK"><img src = {Vk} width = "230px"/></Link></li>
+       <li><Link to="/facebook"><img src = {Face} width = "180px" className="faceInst"/></Link></li>
+       <li><Link to="/instagram"><img src = {Inst} width = "180px"className="faceInst"/></Link></li>
       </ul>
       </div>
     )
